ed20190f84156ae44e6df175a320757708489b09
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-12 23:27:44 +0600

    Add User interface with enums and auth providers
    Define IUser interface structure with related enums (Role, IsActive) and IAuthProvider interface. Includes core user properties, authentication methods, and references to bookings/guides using Mongoose ObjectId. This establishes the foundational type for user data management.

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,34 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+export interface IAuthProvider {
+  provider: string; //"google", "facebook", "email+password"
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVarified?: string;
+  role: Role;
+  auths: IAuthProvider[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
