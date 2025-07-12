38295b3d1955c7e22f333c175f2d5689b57b32cd
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-12 21:21:26 +0600

    Add process error handlers for graceful server shutdown
    Implement error handling for unhandled rejections, uncaught exceptions, and termination signals (SIGTERM/SIGINT) to ensure proper server shutdown. Each handler logs the error, closes the server connection if active, and exits the process with code 1. This improves reliability by preventing zombie processes and ensuring clean exits during unexpected failures or manual shutdowns.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { promise } from "zod";
 
 let server: Server;
-
 
 const startServer = async () => {
   try {
@@ -23,3 +23,68 @@ const startServer = async () => {
 
 startServer();
 
+//1. unhandled rejecton error
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected... Server shutting down", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+//2. uncaught rejection error
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+//3. signal termination (sigterm)
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received... Server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+
+//extra for ourself , when we need to  shoutdown the server manually
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received... Server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+// example 1:
+// Promise.reject(new Error ("I forgot to catch this promise"))
+
+// example 2:
+// throw new Error("I forgot to handle this local error");
+
+/**
+ * *Errors*
+ * 1. unhandled rejecton error
+ * 2. uncaught rejection error
+ * 3. signal termination (sigterm)
+ */
