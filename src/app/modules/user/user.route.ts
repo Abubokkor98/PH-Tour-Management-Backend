5090043ffb2d76ec4f3ee1d10b9d0d723bc8488e
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-14 21:01:50 +0600

    Add request validation to user registration route
    - Import validateRequest middleware and createUserZodSchema
    - Apply validation middleware to POST /register route
    - Add new validation middleware and user schema files
    - Ensure incoming user data meets defined schema requirements

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,11 @@
+import { validateRequest } from "../../middlewares/validateRequest";
 import { UserController } from "./user.controller";
 import { Router } from "express";
+import { createUserZodSchema } from "./user.validation";
 
 const userRoute = Router();
 
-userRoute.post("/register", UserController.createUser);
+userRoute.post("/register",validateRequest(createUserZodSchema),UserController.createUser);
 userRoute.get("/", UserController.getAllUser);
 
 export default userRoute;
