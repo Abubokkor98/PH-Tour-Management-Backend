4afe470696c1e8b010e105c67f21944c32e3a085
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-21 23:16:47 +0600

    29-6 Handling Mongoose Validation Error
    -Handle ValidationError in global error handler
    
    - Add specific error handling for Mongoose ValidationError cases
    - Construct detailed error messages listing all invalid fields and reasons
    - Improve error reporting for client input validation failures
    - Return 400 status code with structured validation error details

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -24,6 +24,14 @@ export const globalErrorHandler = (
   else if (error.name === "CastError") {
     statusCode = 400;
     message = `Invalid ObjectId, please provide a valid ID`;
+  }
+  //Validation Error (type mismatch, required fields, etc.) //* will have to validate mutiple wrong field as well*/
+  else if (error.name === "ValidationError") {
+    statusCode = 400;
+    const errorDetails = Object.values(error.errors).map((err: any) => {
+      return `Field "${err.path}" - ${err.message}`;
+    });
+    message = `Validation failed: ${errorDetails.join("; ")}`;
   } else if (error instanceof AppError) {
     statusCode = error.statusCode;
     message = error.message;
